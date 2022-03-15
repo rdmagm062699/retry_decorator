dcf49f3974e4eb288ce019109a00f7a95cf6ba0a
Rob Miller <rdmagm062699@gmail.com>
2022-03-14 20:53:43 -0500

    Implement exponential backoff for retries

--- src/deco.py
+++ src/deco.py
@@ -1,19 +1,22 @@
 from functools import wraps
-from textwrap import wrap
+from time import sleep
 
-def retry_exception(max_retries):
+def retry_exception(max_retries, backoff=0):
     def retry_decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             tries = 0
             while True:
                 try:
-                    print('blah')
                     func(*args, **kwargs)
+                    break
                 except Exception:
                     tries += 1
                     if tries > max_retries:
-                        break
+                        raise
+                    else:
+                        sleep_time = backoff**(tries - 1)
+                        sleep(sleep_time)
             
         return wrapper
     return retry_decorator
--- test/deco_test.py
+++ test/deco_test.py
@@ -1,14 +1,41 @@
+from ast import Call
+from time import sleep
+import pytest
 from src.deco import retry_exception
-from unittest.mock import Mock
+from unittest.mock import Mock, patch, call
 
 
-def test_retry_exception_retries_designated_number_of_times():
+@patch('src.deco.sleep', autospec=True, spec_set=True)
+def test_retry_exception_retries_designated_number_of_times(sleep_mock):
     mock_function = Mock()
     mock_function.side_effect = Exception('BlahBlahBlah')
+    sleep_mock.side_effect = None
 
     @retry_exception(max_retries=3)
     def some_function():
         mock_function()
 
-    some_function()
+    with pytest.raises(Exception):
+        some_function()
     assert mock_function.call_count == 4
+
+
+@patch('src.deco.sleep', autospec=True, spec_set=True)
+def test_retry_exception_implements_exponential_backoff(sleep_mock):
+    mock_function = Mock()
+    mock_function.side_effect = Exception('BlahBlahBlah')
+    sleep_mock.side_effect = None
+
+    @retry_exception(max_retries=5, backoff=2)
+    def some_function():
+        mock_function()
+
+    with pytest.raises(Exception):
+        some_function()
+
+    expected_calls = [
+        call(1), call(2), call(4), call(8), call(16)
+    ]
+
+    assert sleep_mock.call_count == 5
+    sleep_mock.assert_has_calls(expected_calls)
